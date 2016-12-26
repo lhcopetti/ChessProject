38d6ab2cf41e15a05a95389afc9cba1fce449ee9
lhcopetti <luis.copetti@bematech.com.br>
2016-12-26 18:50:49 -0200

    INT: Removed the need for explicit initialization of array

--- PGNChess/src/main/java/com/copetti/pgnchess/math/UnitVectorInterpoler.java
+++ PGNChess/src/main/java/com/copetti/pgnchess/math/UnitVectorInterpoler.java
@@ -4,30 +4,32 @@ import static com.copetti.pgnchess.math.VectorMath.subtract;
 
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+
+import one.util.streamex.DoubleStreamEx;
 
 public class UnitVectorInterpoler {
 
-	private final Double[] angleArray = { 0.d, 45.d, 90.d, 135.d, 180.d, 225.d, 270.d, 315.d };
-	private final List<Double> angles = Collections.unmodifiableList(Arrays.asList(angleArray));
+	private final double[] angleArray = DoubleStreamEx //
+			.iterate(0d, x -> x + 45) //
+			.takeWhile(x -> x < 360) //
+			.toArray(); //
 
 	public List<Vector> getIntermediatePoints(Vector origin, Vector target) {
 
 		Vector directionVector = subtract(target, origin).unitify();
 
-		if (angles.stream().noneMatch(angle -> DoubleMath.equals(angle, directionVector.getAngle(), 0.001d)))
+		if (Arrays.stream(angleArray).noneMatch(angle -> DoubleMath.equals(angle, directionVector.getAngle(), 0.001d)))
 			return null;
-		
+
 		List<Vector> points = new ArrayList<>();
 
 		Vector counter = VectorMath.add(origin, directionVector);
-		while (!counter.equals(target))
-		{
+		while (!counter.equals(target)) {
 			points.add(new Vector(counter));
 			counter.add(directionVector);
 		}
-		
+
 		return points;
 	}
 
