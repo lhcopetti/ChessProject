fa3c7a07afee51a3d81eb85a6db70d5d82910225
lhcopetti <lhcopetti@gmail.com>
2017-01-04 21:10:45 -0200

    INT: Creation of the ChessMove Commands

---
+++ PGNChess/src/main/java/com/copetti/pgnchess/command/ChessCommand.java
@@ -0,0 +1,20 @@
+package com.copetti.pgnchess.command;
+
+
+public abstract class ChessCommand
+{
+
+	
+	public boolean execute()
+	{
+		if (!canExecute())
+			return false;
+		
+		return doExecute();
+	}
+	
+	public abstract boolean canExecute();
+	public abstract boolean doExecute();
+	
+	public abstract boolean undo();
+}
--- PGNChess/src/main/java/com/copetti/pgnchess/pieces/ChessPieceType.java
+++ PGNChess/src/main/java/com/copetti/pgnchess/pieces/ChessPieceType.java
@@ -14,20 +14,22 @@ import lombok.Getter;
 
 public enum ChessPieceType
 {
-	PAWN("p", new PawnMoveStrategy()), //
-	ROOK("r", new RookMoveStrategy()), //
-	BISHOP("b", new BishopMoveStrategy()), //
-	KNIGHT("n", null), //
-	QUEEN("q", null), //
-	KING("k", null); //
+	PAWN("p", new PawnMoveStrategy(), 1), //
+	ROOK("r", new RookMoveStrategy(), 5), //
+	BISHOP("b", new BishopMoveStrategy(), 3), //
+	KNIGHT("n", null, 3), //
+	QUEEN("q", null, 9), //
+	KING("k", null, -1); //
 
 	private @Getter String pgnFormat;
 	private ChessMoveStrategy chessMove;
+	private @Getter int score;
 
-	private ChessPieceType(String pgnFormat, ChessMoveStrategy chessMove)
+	private ChessPieceType(String pgnFormat, ChessMoveStrategy chessMove, int score)
 	{
 		this.pgnFormat = pgnFormat;
 		this.chessMove = chessMove;
+		this.score = score;
 	}
 
 	public Set<ChessSquare> getMoves(ChessSquare self, ChessBoard board)
