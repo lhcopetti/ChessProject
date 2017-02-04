f3c44112de1fddd7575f3bf3d43785cbf4421146
Luis Copetti <lhcopetti@gmail.com>
2017-02-04 11:42:08 -0200

    INT: Any *.pgn file inside resources/PGNGames will be tested to conclusion

--- PGNInterpreter/src/test/java/com/copetti/pgn/lichess/LichessGameTester.java
+++ PGNInterpreter/src/test/java/com/copetti/pgn/lichess/LichessGameTester.java
@@ -1,9 +1,8 @@
 package com.copetti.pgn.lichess;
 
+import java.io.File;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.Arrays;
-import java.util.List;
 
 import org.junit.Test;
 
@@ -14,20 +13,27 @@ public class LichessGameTester {
 	@Test
 	public void testPGNGames() throws Exception {
 
-		String firstGame = "lichess_pgn_2016.11.27_lhcopetti_vs_cap1.urXzcfwv.pgn";
-		String secondGame = "lichess_pgn_2017.02.04_lichess_AI_level_2_vs_lhcopetti.phybhkAw.pgn";
+		File[] pgnFiles = new File("./" + rootDirectory).listFiles();
 
-		List<String> pgnGamesToTest = Arrays.asList(firstGame, secondGame);
+		System.out.println("Testing " + pgnFiles.length + " PGN matches");
 
-		for (String s : pgnGamesToTest) {
-			String pgnGame = loadPGNGame(s);
-			LichessPGNGamePlayer.testGameToConclusion(LichessPGNReader.read(pgnGame));
+		for (File f : pgnFiles) {
+			if (f.getName().endsWith(".pgn"))
+				testLichessPGNGame(f);
+			else
+				System.out.println("Skipping file because it is not in PGN format. " + f.getName());
 		}
+	}
+
+	private void testLichessPGNGame(File pgnFile) throws Exception {
+
+		String pgnGame = loadPGNGame(pgnFile.getAbsolutePath());
+		LichessPGNGamePlayer.testGameToConclusion(LichessPGNReader.read(pgnGame));
 	}
 
 	public String loadPGNGame(String filePath) throws Exception {
 
-		return new String(Files.readAllBytes(Paths.get(rootDirectory + filePath)));
+		return new String(Files.readAllBytes(Paths.get(filePath)));
 	}
 
 }
