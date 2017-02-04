7c6a5d527d5266faab62d8b4cc64e4d5dcacd5e8
Luis Copetti <lhcopetti@gmail.com>
2017-02-04 10:54:54 -0200

    INT: Add game to test the chess replayer

--- PGNInterpreter/src/test/java/com/copetti/pgn/lichess/LichessGameTester.java
+++ PGNInterpreter/src/test/java/com/copetti/pgn/lichess/LichessGameTester.java
@@ -2,23 +2,32 @@ package com.copetti.pgn.lichess;
 
 import java.nio.file.Files;
 import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
 
 import org.junit.Test;
 
 public class LichessGameTester {
 
+	private static final String rootDirectory = "src/test/resources/PGNGames/";
+
 	@Test
 	public void testPGNGames() throws Exception {
 
-		String fileName = "src/test/resources/PGNGames/lichess_pgn_2016.11.27_lhcopetti_vs_cap1.urXzcfwv.pgn";
-		String pgnGame = loadPGNGame(fileName);
+		String firstGame = "lichess_pgn_2016.11.27_lhcopetti_vs_cap1.urXzcfwv.pgn";
+		String secondGame = "lichess_pgn_2017.02.04_lichess_AI_level_2_vs_lhcopetti.phybhkAw.pgn";
 
-		LichessPGNGamePlayer.testGameToConclusion(LichessPGNReader.read(pgnGame));
+		List<String> pgnGamesToTest = Arrays.asList(firstGame, secondGame);
+
+		for (String s : pgnGamesToTest) {
+			String pgnGame = loadPGNGame(s);
+			LichessPGNGamePlayer.testGameToConclusion(LichessPGNReader.read(pgnGame));
+		}
 	}
 
 	public String loadPGNGame(String filePath) throws Exception {
 
-		return new String(Files.readAllBytes(Paths.get(filePath)));
+		return new String(Files.readAllBytes(Paths.get(rootDirectory + filePath)));
 	}
 
 }
